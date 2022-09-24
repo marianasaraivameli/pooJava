834cef16e4d68b5d2261f0358f08236deccf3f8a
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-24 09:01:54 -0300

    feat: pooJava02 - Pratica integradora 1 - exercicio 5

--- src/poojava02/Main.java
+++ src/poojava02/Main.java
@@ -3,8 +3,12 @@ package poojava02;
 public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        Pessoa pessoa1 = new Pessoa("Maria", 25, "MG4521");
-        Pessoa pessoa2 = new Pessoa("João", 20, "SP1371", 70, 1.75);
+        Pessoa pessoa1 = new Pessoa("Maria", 17, "MG4521");
+        Pessoa pessoa2 = new Pessoa("João", 25, "SP1371", 50, 1.55);
         // não é possível criar um novo objeto usando apenas um valor para nome e idade, uma vez que não temos um construtor apenas com estes parametros
+
+        System.out.println(pessoa2.calcularIMC());
+        System.out.println(pessoa1.ehMaiorIdade());
+        System.out.println(pessoa2.ehMaiorIdade());
     }
 }
--- src/poojava02/Pessoa.java
+++ src/poojava02/Pessoa.java
@@ -1,15 +1,41 @@
 package poojava02;
 
 public class Pessoa {
-    String nome;
-    int idade;
-    String ID;
-    int peso;
-    double altura;
+    private String nome;
+    private int idade;
+    private String ID;
+    private double peso;
+    private double altura;
 
     public Pessoa() {}
 
-    public Pessoa(String nome, int idade, String ID) {}
+    public Pessoa(String nome, int idade, String ID) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+    }
 
-    public Pessoa(String nome, int idade, String ID, int peso, double altura) {}
+    public Pessoa(String nome, int idade, String ID, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularIMC () {
+        double resultIMC = peso / ( altura * altura);
+        System.out.println(resultIMC);
+        if (resultIMC < 20) {
+            return -1;
+        }
+        if (resultIMC >=20 && resultIMC <=25) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public boolean ehMaiorIdade() {
+        return idade >=18;
+    }
 }
