194dbdcd4e623429fefbb85dee9a329901dc82d9
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-24 08:20:01 -0300

    feat: pooJava02 - Pratica integradora 1 - exercicio 2

---
+++ src/poojava02/Pessoa.java
@@ -0,0 +1,15 @@
+package poojava02;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    String ID;
+    int peso;
+    double altura;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, int idade, String ID) {}
+
+    public Pessoa(String nome, int idade, String ID, int peso, double altura) {}
+}
