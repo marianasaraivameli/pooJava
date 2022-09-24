3a4f79bae6b4bca942c836bcb236c05f58b7c3f1
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-24 12:36:18 -0300

    feat: pooJava02 - Pratica integradora 2 - exercicio 2 parte c

---
+++ src/poojava02parte02exercicio02/NaoPereciveis.java
@@ -0,0 +1,25 @@
+package poojava02parte02exercicio02;
+
+public class NaoPereciveis extends Produto {
+    private String tipo;
+
+    public NaoPereciveis(String tipo, String nome, double preco) {
+        super(nome, preco);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "NaoPereciveis{" +
+                "tipo='" + tipo + '\'' +
+                '}';
+    }
+}
