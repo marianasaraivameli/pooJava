aa2e01c46016aef86a8b2897d13673a50d93619c
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-26 20:27:57 -0300

    feat: pooJava03 - Pratica integradora 1 - exercicio 1

---
+++ src/poojava03praticaIntegradora01/ClienteCobrador.java
@@ -0,0 +1,25 @@
+package poojava03praticaIntegradora01;
+
+public class ClienteCobrador extends OperacoesBanco{
+    String nome;
+
+    public ClienteCobrador(String nome) {
+        super();
+        this.nome = nome;
+    }
+
+    @Override
+    public void saqueDinheiro(int num) {
+        super.saqueDinheiro(num);
+    }
+
+    @Override
+    public void consultaSaldo(int num) {
+        super.consultaSaldo(num);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
---
+++ src/poojava03praticaIntegradora01/ClienteExecutivo.java
@@ -0,0 +1,25 @@
+package poojava03praticaIntegradora01;
+
+public class ClienteExecutivo extends OperacoesBanco{
+    String nome;
+
+    public ClienteExecutivo(String nome) {
+        super();
+        this.nome = nome;
+    }
+
+    @Override
+    public void deposito(int num) {
+        super.deposito(num);
+    }
+
+    @Override
+    public void transferencia(int num) {
+        super.transferencia(num);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
