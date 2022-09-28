24a592380b22cd8f37e1da2c27267746dd164198
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-28 16:05:00 -0300

    feat: projeto aula ao vivo - class Gerenciar conta, NumberGenerator e ExceptionConta

--- src/aula04/aulaAoVivo/Main.java
+++ src/aula04/aulaAoVivo/Main.java
@@ -1,8 +1,25 @@
 package aula04.aulaAoVivo;
 
 
-public class Main {
-    public static void main(String[] args) {
+import aula04.aulaAoVivo.data.GerenciaContas;
+import aula04.aulaAoVivo.exception.ContaInexistenteException;
+import aula04.aulaAoVivo.modelo.Cliente;
 
+public class Main {
+    private static GerenciaContas contas = new GerenciaContas();
+    public static void main(String[] args) {
+        contas.novaContaCorrente(new Cliente());
+        contas.novaContaCorrente(new Cliente());
+        contas.novaContaCorrente(new Cliente());
+
+        try {
+            contas.depositar(1, 100);
+            contas.transferir(1, 2, 50);
+            System.out.println(contas.obterDadosConta(1));
+            System.out.println(contas.obterDadosConta(2));
+            System.out.println(contas.obterDadosConta(3));
+        } catch (Exception e) {
+            System.out.println("Erro");
+        }
     }
 }
